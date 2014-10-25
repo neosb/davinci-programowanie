a4483726653af33c960f222a502af5a64242cd61
Szymon Błaszczyński <simone@neosb.net>
2014-10-25 16:20:52 +0200

    My name, FTW!

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <conio.h>
+
+using namespace std;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	
+	cout << "Szymon Blaszczynski" << endl;
+	getch();
 	return 0;
 }
